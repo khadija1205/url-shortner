3e62a232e2fb1466c2b4daf625e012a28d497c7c
khadija1205 <khadijac101@gmail.com>
2025-07-01 00:15:36 +0530

    created an api for signing up the user

--- src/db/apiAuth.jsx
+++ src/db/apiAuth.jsx
@@ -1,6 +1,6 @@
 // all logic related to login and signup
 
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function login({ email, password }) {
     const {data, error} = await supabase.auth.signInWithPassword({
@@ -19,4 +19,29 @@ export async function getCurrentUser() {
     if (!session.session) return null;
     if (error) throw new Error(error.message);
     return session.session?.user;
+}
+
+
+// api for signing up the user
+
+export async function signup({ name, email, password, profile_pic }) {
+    const fileName = `dp-${name.split(" ").join("-")}-${Math.random()}`;
+    const { error: storageError } = await supabase.storage.from("profile-pic").upload(fileName, profile_pic);
+
+    if (storageError) throw new Error(storageError.message);
+
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            data: {
+                name,
+                profile_pic: `${supabaseUrl}/storage/v1/object/public/profile-pic/${fileName}`,
+            }
+        }
+    });
+
+    if (error) throw new Error(error.message);
+
+    return data;
 }
