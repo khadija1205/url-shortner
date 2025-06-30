eaecee1c13b222646886308f0d9397232cf5ea66
khadija1205 <khadijac101@gmail.com>
2025-06-30 20:21:23 +0530

    added login component to auth page

---
+++ src/components/login.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { BeatLoader } from 'react-spinners';
+
+const Login = () => {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Login</CardTitle>
+                <CardDescription>to your account if you already have one</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+                <div className="space-y-1">
+                    <Input name="email" type="email" placeholder="Enter Email" />
+                </div>
+                <div className="space-y-1">
+                    <Input name="password" type="password" placeholder="Enter Password" />
+                </div>
+            </CardContent>
+            <CardFooter>
+                <Button>{true ? <BeatLoader size={10} color='#36d7b7'/> : 'Login'}</Button>
+            </CardFooter>
+        </Card>
+    );
+};
+
+export default Login;
