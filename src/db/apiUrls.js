a7ecaaca28458ecd13537a0c6042493a4033cf5d
khadija1205 <khadijac101@gmail.com>
2025-07-09 21:05:55 +0530

    Added statistics and UI for link page

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -1,3 +1,4 @@
+import { UAParser } from "ua-parser-js";
 import supabase, { supabaseUrl } from "./supabase";
 
 
@@ -56,3 +57,32 @@ export async function createUrl({ title, longUrl, customUrl, user_id }, qrcode) 
 
     return data;
 }
+
+
+export async function getLongUrl(id) {
+    let { data, error } = await supabase.from('urls').select("id, original_url").or(`short_url.eq.${id},custom_url.eq.${id}`).single();
+
+    if (error) {
+        console.error(error.message);
+        throw new Error('Error fetching short link');
+    }
+
+    return data;
+}
+
+export async function getUrl({id, user_id}) {
+    let { data, error } = await supabase
+        .from('urls')
+        .select("*")
+        .eq("id", id)
+        .eq("user_id", user_id)
+        .single();
+
+    if (error) {
+        console.error(error.message);
+        throw new Error('Short Url not found');
+    }
+
+    return data;
+}
+
